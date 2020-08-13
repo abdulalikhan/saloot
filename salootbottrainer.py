a7fb200e85ad54ad6cb32efdab4d5d8052fcaacb
Abdul Ali Khan <alikhan@cocashcreators.com>
2020-08-13 22:37:37 +0500

    Replaced the deprecated "set_trainer" method

--- salootbottrainer.py
+++ salootbottrainer.py
@@ -2,5 +2,5 @@ from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
 
 chatbot = ChatBot('saloot')
-chatbot.set_trainer(ChatterBotCorpusTrainer)
-chatbot.train("chatterbot.corpus.english")
+trainer = ChatterBotCorpusTrainer(chatbot)
+trainer.train("chatterbot.corpus.english")
